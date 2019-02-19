7a444d7b4af12e73977a013755da7181b18ea26f
Julia Wharton <ju-wharton@live.co.uk>
2019-02-19 14:44:48 -0300

    Iniciar código do jogo da forca

---
+++ script.js
@@ -0,0 +1,8 @@
+let listaPalavras = [[D, E, S, G, R, A, Ç, A,], [D, E, S, P, R, E, Z, O]];
+
+let palavraGerada = listaPalavras[Math.floor(Math.random()*listaPalavras.length)];
+
+let guess = prompt('Digite uma letra.')
+
+palavraGerada = document.querySelectorAll('p');
+
